56030bed412665d78656936365db9ae89ff77dfe
shiho-aoki <s.aoki.ase@gmail.com>
2022-06-08 02:28:04 +0900

    add; header and directory conv.

--- ui/src/router.ts
+++ ui/src/router.ts
@@ -3,12 +3,29 @@ import * as VueRouter from 'vue-router';
 import { createRouter, createWebHistory } from 'vue-router'
 
 // views import
-import Home from './views/Home.vue';
-import About from './views/About.vue';
+// -top level componets
+import Home from './views/Home.vue'; //Latest Activity
+import TechNotes from './views/TechNote/TechNotes.vue';
+import Projects from './views/Projects/Projects.vue';
+import Timeline from './views/Timeline/Timeline.vue';
+import About from './views/Profile/About.vue';
+
+// Pages
+// -about
+import Academic from './views/Profile/Academic.vue';
+import Work from './views/Profile/Work.vue';
 
 const routes = [
-    {path: '/', component: Home },
+    //Top level Components
+    {path: '/', component: Home }, //Latest Activity Top
+    {path: '/tech_note', component: TechNotes},
+    {path: '/project', component: Projects },
+    {path: '/timeline', component:Timeline},
     {path: '/about',component: About},
+    //Pages
+    // -about
+    {path: '/work', component: Work},
+    {path: '/academic', component: Academic}
 ]
 
 const router = createRouter({
