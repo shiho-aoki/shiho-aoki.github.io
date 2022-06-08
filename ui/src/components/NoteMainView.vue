83e395eca5d974e4fab43689debf3e790c3ad845
shiho-aoki <s.aoki.ase@gmail.com>
2022-06-08 15:47:47 +0900

    add; main and sub view on blog, and this will be fxed

--- ui/src/components/NoteMainView.vue
+++ ui/src/components/NoteMainView.vue
@@ -1,3 +1,12 @@
+<script setup lang="ts">
+defineProps<{
+    mainItem: any
+}>();
+</script>
 <template>
-<p>一応ここがメインフレーム</p>
+    <div class="mt-2 ml-5" v-for="item in mainItem">
+      <h1 class="text-2xl font-extrabold text-gray-900">
+        <a :href="item.itemULR">{{item.itemName}}</a>
+      </h1>
+    </div>
 </template>
--- ui/src/views/TechNote/TechNotes.vue
+++ ui/src/views/TechNote/TechNotes.vue
@@ -1,9 +1,50 @@
 <template>
-    <SideBar />
-    <div class="lg:ml-80 portrait:ml-10">
-      <PageTitle title="Tech Note" />
+    <SideBar class="portrait:hidden" :subItems="SidebarItems" />
+    <div class="p-2 lg:ml-80 portrait:ml-8">
+      <NoteMainView :mainItem="onlyMainItemsForTopMainView" />
     </div>
 </template>
+
+<script setup lang="ts">
+
+const onlyMainItemsForTopMainView = [
+  {
+    itemName: "Vue",
+    itemULR: "#"
+  },
+    {
+    itemName: "Vue2",
+    itemULR: "#"
+  }
+]
+
+const subItemList = [ 
+  {
+    itemName: "data1",
+    itemURL: "#"
+  },
+  {
+    itemName: "data2",
+    itemURL: "#"
+  },
+  {
+    itemName: "data3",
+    itemURL: "#"
+  }
+]
+const SidebarItems = [
+  {
+    mainItem: "Vue",
+    mainItemURL: "#",
+    subItems: subItemList
+  },
+  {
+    mainItem: "Vue2",
+    mainItemURL: "#",
+    subItems: subItemList
+  }
+]
+</script>
 
 <script lang="ts">
 // @ is an alias to /src
