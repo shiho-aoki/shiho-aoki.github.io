df5a9596c3c8a03fff8d3e338f286ce1e1cabf94
shiho-aoki <s.aoki.ase@gmail.com>
2022-06-28 11:29:26 +0900

    add; pages dir for mpa

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import mpa from 'vite-plugin-mpa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,6 +8,11 @@ export default defineConfig({
     ? '/shiho-aoki/ui/' : './',
   build: {
    outDir: '../docs'
- },
-  plugins: [vue()]
+  },
+  plugins: [vue(), mpa()],
+  // resolve: {
+  //   alias: {
+  //     '@': path.resolve(__dirname, './src')
+  //   }
+  // }
 })
