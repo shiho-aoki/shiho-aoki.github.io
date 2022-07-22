ee0dcc91ec66ead6acf61c026ccf40493ce30765
shiho-aoki <s.aoki.ase@gmail.com>
2022-07-23 08:42:19 +0900

    add; mpa template

---
+++ view/src/components/views/ProfileView.vue
@@ -0,0 +1,12 @@
+<template>
+    <h2>
+        yes
+    </h2>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: 'ProfileView',
+});
+</script>
---
+++ view/src/pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <Layer pageTitle="shiho-aoki" />
+  <ProfileView />
+</template>
+
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+import Layer from "../components/globals/layer.vue";
+import ProfileView from "../components/views/ProfileView.vue";
+
+export default defineComponent({
+  name: 'Profile',
+  components: {
+    Layer,
+    ProfileView
+  }
+});
+</script>
