2e9d26b2ca1e1cb8e9d3e9f74cc2db3d23cb827b
shiho-aoki <s.aoki.ase@gmail.com>
2023-08-23 15:01:10 +0900

    update index page

--- view/src/pages/profile/App.vue
+++ view/src/pages/profile/App.vue
@@ -2,21 +2,33 @@
   <Header />
   <div class="bg-gray-100">
     <div class="mx-auto max-w-7xl py-12 px-4 sm:px-6 lg:flex lg:items-center lg:justify-between lg:py-16 lg:px-8">
-      <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-        <span class="block">shiho-aoki @as.+?pml</span>
-        <img src="https://github-readme-stats.vercel.app/api?username=shiho-aoki&count_private=true&show_icons=true" 
-              class="h-auto w-full my-3" />
-        <span class="text-lg font-semibold leading-8 text-blue-500">If I have seen further it is by standing on yᵉ sholders of Giants.</span>
+      <h2 class="text-4xl tracking-tight text-gray-900 sm:text-5xl text-align: center">
+        <span class="block">shiho aoki</span>
+        <h3 class="mt-2 flex text-sm text-gray-500">
+          早稲田大学先進理工学研究科 一貫制博士課程<br>
+          IATD. Co., Ltd.(株式会社 先端技術開発研究所) CTO
+        </h3>
+        <span class="mt-2 flex text-sm text-gray-500">
+          <TagIcon class="mr-1.5 h-5 w-5 flex-shrink-0 text-gray-400" aria-hidden="true" />
+          Biotechnology, Development, Computer Science - Software Engineering, Research and Develoment
+        </span>
+        <!-- <img src="https://github-readme-stats.vercel.app/api?username=shiho-aoki&count_private=true&show_icons=true" 
+              class="h-auto w-full my-3" /> -->
+        <span class="text-lg font-semibold text-blue-500">
+          If I have seen further it is by standing on yᵉ sholders of Giants.
+        </span>
       </h2>
       <div class="mt-8 flex lg:mt-0 lg:flex-shrink-0">
         <div class="inline-flex rounded-md shadow">
           <a :href="url.profile.academic" class="inline-flex items-center justify-center rounded-md border border-transparent bg-blue-500 px-5 py-3 text-base font-medium text-white hover:bg-blue-600">
-            academic
+            >> Academic <br>
+            (Researcher Profile)
           </a>
         </div>
         <div class="ml-3 inline-flex rounded-md shadow">
           <a :href="url.profile.business" class="inline-flex items-center justify-center rounded-md border border-transparent bg-white px-5 py-3 text-base font-medium text-blue-500 hover:bg-blue-50">
-            business
+            >> Business <br>
+            (Engineer Profile)
           </a>
         </div>
       </div>
@@ -25,20 +37,83 @@
   <div calss="mx-auto max-w-7xl py-12 px-4 sm:px-6 lg:flex lg:items-center lg:justify-between lg:py-16 lg:px-8">
     <div class="px-4">
       <span class="mt-6 max-w-2xl text-lg leading-8 text-gray-600">
-        <br />
-        Experienced Software Engineer with a demonstrated history of working in the higher education industry. <br />
-        Skilled in Python, JavaScript/TypeScript (React, Next, Angular, Vue, Nuxt), Golang, Elang, C++ ,and Google Cloud Platform, AWS etc.<br />
-        Engineering professional with a Bachelor of Science - BS focused in Biomedical/Medical Engineering from Waseda University.<br />
+        <div class="py-16 sm:px-0">
+          <TabGroup>
+            <TabList class="flex space-x-1 rounded-xl bg-blue-900/20 p-1">
+              <Tab
+                v-for="category in Object.keys(categories)"
+                as="template"
+                :key="category"
+                v-slot="{ selected }"
+              >
+                <button
+                  :class="[
+                    'w-full rounded-lg py-2.5 text-sm font-medium leading-5 text-blue-700',
+                    'ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2',
+                    selected
+                      ? 'bg-white shadow'
+                      : 'text-blue-100 hover:bg-white/[0.12] hover:text-white',
+                  ]"
+                >
+                  {{ category }}
+                </button>
+              </Tab>
+            </TabList>
+            <TabPanels class="mt-2">
+              <TabPanel
+                v-for="(posts, idx) in Object.values(categories)"
+                :key="idx"
+                :class="[
+                  'rounded-xl bg-white p-3',
+                  'ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2',
+                ]"
+              >
+                <ul>
+                  <li
+                    v-for="post in posts"
+                    :key="post.id"
+                    class="relative rounded-md p-3 hover:bg-gray-100"
+                  >
+                    <h3 class="text-sm font-medium leading-5">
+                      {{ post.title }}
+                    </h3>
+
+                    <ul
+                      class="mt-1 flex space-x-1 text-xs font-normal leading-4 text-gray-500"
+                    >
+                      <li>{{ post.date }}</li>
+                      <li>&middot;</li>
+                      <li>{{ post.commentCount }} comments</li>
+                      <li>&middot;</li>
+                      <li>{{ post.shareCount }} shares</li>
+                    </ul>
+                    <a
+                      href="#"
+                      :class="[
+                        'absolute inset-0 rounded-md',
+                        'ring-blue-400 focus:z-10 focus:outline-none focus:ring-2',
+                      ]"
+                    />
+                  </li>
+                </ul>
+              </TabPanel>
+            </TabPanels>
+          </TabGroup>
+        </div>
       </span>
     </div>
   </div>
 
 
-  <div class="flex justify-center mt-10">
-    <div class="grid grid-cols-2 gap-4">
+  <!-- <div class="flex justify-center mt-10">
+    <div class="grid grid-cols-2 gap-4"> -->
+
+    <!-- Did not use -->
     <!-- <img src="http://github-profile-summary-cards.vercel.app/api/cards/profile-details?username=shiho-aoki&theme=github_dark" 
            class="h-auto w-full object-cover object-center"/> -->
-      <div class="col-span-2">
+    <!-- Did not use -->
+
+      <!-- <div class="col-span-2">
         <img src="http://github-profile-summary-cards.vercel.app/api/cards/profile-details?username=shiho-aoki&theme=github_dark" />
       </div>
       <div>
@@ -48,13 +123,16 @@
         <img src="http://github-profile-summary-cards.vercel.app/api/cards/productive-time?username=shiho-aoki&theme=github_dark&utcOffset=9" />
       </div>
     </div>
-  </div>
+  </div> -->
+
+  <!-- Did not use -->
   <!-- <div class="mx-32 px-32">
     <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=shiho-aoki&count_private=true&hide=jupyter%20notebook,html,CSS,Makefile&langs_count=11&layout=compact" 
         class="h-auto w-full object-cover object-center" />
   </div> -->
+  <!-- Did not use -->
 
-  <div class="bg-white py-24 sm:py-32 lg:py-40">
+  <!-- <div class="bg-white py-24 sm:py-32 lg:py-40">
     <div class="mx-auto max-w-7xl px-6 lg:px-8 content-center">
       <div class="sm:text-center">
         <h2 class="text-lg font-semibold leading-8 text-blue-500">skills</h2>
@@ -67,24 +145,30 @@
       </div>
     </div>
   </div>
-  <Skills />
+  <Skills /> -->
 
-  <div class="bg-white py-24 sm:py-32 lg:py-40">
+  <!-- <div class="bg-white py-24 sm:py-32 lg:py-40">
     <div class="mx-auto max-w-7xl px-6 lg:px-8 content-center">
       <div class="sm:text-center">
         <h2 class="text-lg font-semibold leading-8 text-blue-500">since 2017.</h2>
-        <p class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Licenses and Certifications</p>
+        <p class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Licenses and Certifications</p> -->
+
+        <!-- Did not use -->
         <!-- <div class="flex justify-center mt-4">
           <a :href="url.other.github" target="_blank" rel="noopener noreferrer">
             <img src="https://img.shields.io/github/followers/shiho-aoki?label=Follow&style=social" />
           </a>
         </div> -->
-      </div>
+        <!-- Did not use -->
+
+      <!-- </div>
     </div>
-  </div>
-  <div class="justify-center mb-6 lg:mx-32 sm:mx-3">
+  </div> -->
+
+  <!-- <div class="justify-center mb-6 lg:mx-32 sm:mx-3">
     <LicenssesAndCertifications />
-  </div>
+  </div> -->
+  
   <Footter />
 </template>
 
@@ -94,21 +178,27 @@ import Footter from '../../components/Footter.vue';
 import Skills from './Skills.vue';
 import LicenssesAndCertifications from './LicenssesAndCertifications.vue'
 import { urls } from '../../store/urls';
+import { myprofile } from '../../store/profile/myprofile';
 import { ref } from 'vue';
+import { TagIcon } from '@heroicons/vue/24/outline';
+import { TabGroup, TabList, Tab, TabPanels, TabPanel } from '@headlessui/vue'
 
 export default {
   name: 'Profile',
   setup(){
     const url = ref(urls);
+    const categories = ref(myprofile);
     return {
-      url
+      url,
+      categories
     }
   },
   components: {
     Header,
     Skills,
     LicenssesAndCertifications,
-    Footter
+    Footter,
+    TagIcon, TabGroup, TabList, Tab, TabPanels, TabPanel
   }
 }
 </script>
